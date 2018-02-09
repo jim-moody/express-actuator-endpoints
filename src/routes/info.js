9225be79d1bd6758ef4e5502a3dd11e7e2b331ba
JIM MOODY <jim.moody@libertymutual.com>
2018-02-09 16:32:50 -0500

    Add git info to /info route

--- src/routes/info.js
+++ src/routes/info.js
@@ -1,8 +1,19 @@
-const appRoot =require("app-root-path")
-const gitInfo = require(appRoot + "/git.properties.json";
+let gitInfo;
+try {
+  const appRoot = require("app-root-path");
+  gitInfo = require(`${appRoot}/git.properties.json`);
+} catch (e) {
+  console.log(
+    "Add a git.properties.json to serve up git info at this endpoint"
+  );
+}
 
 export default (req, res) => {
+  const info = {};
+  if (gitInfo) {
+    info.git = gitInfo;
+  }
   res.json({
-    git: gitInfo
+    info
   });
 };
